d0b1558031bb995c02ebfba3a208c066514a8405
AndGasper <andy.gasper1@gmail.com>
2017-07-20 19:39:52 -0700

    Added command line parameters example

---
+++ java_tutorial/UseArgument.java
@@ -0,0 +1,10 @@
+/**
+ * Compilation: javac UseArgument.java
+ * Execution java UseArgument name
+ */
+
+ public class UseArgument {
+     public static void main(String[] args) {
+         System.out.println("Hello,"+args[0]+args[1]);
+     }
+ }
